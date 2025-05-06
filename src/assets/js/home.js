0601199487e03deec86e29b63d4305c288f84a21
28963_icfcorp <28963@icf.com>
2025-05-06 06:40:21 -0400

    main push

---
+++ src/assets/js/home.js
@@ -0,0 +1,18 @@
+import gsap  from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline({ defaults: { duration: 0.75, ease: "power1.out" } });
+
+tl.to('.line', {
+  scrollTrigger: {
+    trigger: ".work",
+    start: "top top",
+    end: "bottom top",
+    scrub: true,
+    pin: true,
+    markers: true
+  },
+  width: '100%',
+  stagger: 0.25
+});
